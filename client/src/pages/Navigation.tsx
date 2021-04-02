3fd8359b3c94bd19ae7f2613ac2bc8b4204f828d
Chris <cdhenry@Chriss-MBP.lan>
2021-04-02 17:56:33 -0500

    fix (wip): site navigation using react router

--- client/src/pages/Navigation.tsx
+++ client/src/pages/Navigation.tsx
@@ -2,32 +2,35 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBook, faHome, faMapMarkerAlt, faMicrophone, faPen, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { RoutesEnum } from '../enums/routes';
 
 library.add(faBook, faHome, faMicrophone, faPen, faMapMarkerAlt, faTimes)
 
 export default function Navigation(props: { handleCloseNav: () => void }): JSX.Element {
     const defaultClassName =
         'relative my-2.5 flex justify-around items-center p-5 min-w-1/2 border border-2 border-black rounded bg-gray-50 shadow-lg'
-    const buttonClassName =
+    const linkClassName =
         'hover:transform hover:scale-150 transition delay-100 duration-200 ease-in ease-out active:text-gray-500'
 
     return (
         <section className={defaultClassName}>
-            <button className={buttonClassName}>
+            <Link to={RoutesEnum.Home} className={linkClassName}>
                 <FontAwesomeIcon icon={faHome} size="lg" />
-            </button>
-            <button className={buttonClassName}>
+            </Link>
+            <Link to={RoutesEnum.Library} className={linkClassName}>
                 <FontAwesomeIcon icon={faBook} size="lg" />
-            </button>
-            <button className={buttonClassName}>
+            </Link>
+            <Link to={RoutesEnum.Theater} className={linkClassName}>
                 <FontAwesomeIcon icon={faMicrophone} size="lg" />
-            </button>
-            <button className={buttonClassName}>
+            </Link>
+            <Link to={RoutesEnum.Studio} className={linkClassName}>
                 <FontAwesomeIcon icon={faPen} size="lg" />
-            </button>
-            <button className={buttonClassName}>
+            </Link>
+            <Link to={RoutesEnum.Map} className={linkClassName}>
                 <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" />
-            </button>
+            </Link>
             <button className="absolute right-3 top-0.5" onClick={props.handleCloseNav}>
                 <FontAwesomeIcon icon={faTimes} size="xs" />
             </button>
