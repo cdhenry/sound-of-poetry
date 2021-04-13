6346bfb7a6f1603a50b1fd210cc96eafd72f5ef1
Chris <cdhenry@Chriss-MBP.lan>
2021-04-13 06:49:02 -0500

    fix (temp):  commented out freesound configuration

--- client/src/services/sound.ts
+++ client/src/services/sound.ts
@@ -1,17 +1,18 @@
 import axios from 'axios';
-import FreeSound from 'freesound-client';
 
-import tokens from '../common/assets/config-tokens';
 import { IPaginatedList, IPaginatedParams } from '../interfaces/shared';
 import { ISound } from '../interfaces/sound';
 
+// Commented text to be added when sound is incorporated
+// import FreeSound from 'freesound-client';
+// import tokens from '../common/assets/config-tokens';
 export class SoundService {
     private _baseUrl: string = 'sound/'
-    private _freeSound = new FreeSound()
+    // private _freeSound = new FreeSound()
 
-    constructor() {
-        this._freeSound.setToken(tokens.freesound)
-    }
+    // constructor() {
+    //     this._freeSound.setToken(tokens.freesound)
+    // }
 
     public async getSounds(params: IPaginatedParams) {
         let response = await axios.get(`${this._baseUrl}`, { params })
@@ -23,10 +24,10 @@ export class SoundService {
         return response.data as ISound
     }
 
-    public async getFreeSound(soundId: number) {
-        const response = await this._freeSound.getSound(soundId)
-        return response
-    }
+    // public async getFreeSound(soundId: number) {
+    //     const response = await this._freeSound.getSound(soundId)
+    //     return response
+    // }
 }
 
 export const soundService = new SoundService()
