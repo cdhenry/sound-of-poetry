1071215964fa7e3b4e320c80568bcb9268102636
Shengyun Huang <wshy@seas.upenn.edu>
2021-04-21 15:54:35 -0400

    added queries for word tab

--- server/routes/words.js
+++ server/routes/words.js
@@ -67,4 +67,74 @@ router.get("/:word", function (req, res) {
   });
 });
 
+// frequently used words to display on the page
+router.get("/frequent", function (req, res) {
+  var query = `
+    SELECT w.lemma 
+    FROM poem_wordnet pw
+    INNER JOIN words w ON pw.word_id = w. wordid
+    WHERE length(w.lemma) >4
+    ORDER BY use_count DESC 
+    LIMIT 20
+  `;
+  connection.query(query, function (err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+});
+
+// poem words with image  
+router.get("/hasimage", function (req, res) {
+  var query = `
+    SELECT lemma 
+    FROM poem_wordnet p
+    INNER JOIN wordsXsensesXsynsets w ON p.word_id=w.wordid
+    INNER JOIN google_images_synsets g ON w.synsetid=g.synsetid
+    INNER JOIN images_synsets i ON i.synsetid = g.synsetid
+  `;
+  connection.query(query, function (err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+});
+
+// poem words with youtube  
+router.get("/hasyoutube", function (req, res) {
+  var query = `
+    SELECT lemma
+    FROM poem_wordnet p 
+    INNER JOIN wordsXsensesXsynsets w ON p.word_id=w.wordid
+    INNER JOIN media_class_wordnet m ON m.word_id=p.word_id
+    INNER JOIN ytid_mid y ON m.m_id=y.m_id
+  `;
+  connection.query(query, function (err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+});
+
+// poems with the lemma (from image or sound search)
+router.get("/:lemma", function (req, res) {
+  var lemma = req.params.word;
+  var query = `
+    SELECT p.title, p.poem_string
+    FROM poem p
+    JOIN poem_wordnet pw ON p.id=pw.poem_id
+    INNER JOIN wordsXsensesXsynsets w ON pw.word_id=w.wordid
+    WHERE lemma = ${lemma};
+  `;
+  connection.query(query, function (err, rows, fields) {
+    if (err) console.log(err);
+    else {
+      res.json(rows);
+    }
+  });
+});
+
 module.exports = router;
