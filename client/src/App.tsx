0ecfdeb195bf78f380d9a13d512b644a82a51fd8
Chris <cdhenry@Chriss-MBP.lan>
2021-04-21 07:10:51 -0500

    fix: nav + drawer z index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import { createBrowserHistory } from 'history';
-import React from 'react';
-import { Router } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { createBrowserHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router-dom'
 
-import Drawer from './pages/Drawer';
-import Navigation from './pages/Navigation';
-import Routes from './Routes';
+import Drawer from './pages/Drawer'
+import Navigation from './pages/Navigation'
+import Routes from './Routes'
 
 library.add(faTimes)
 
@@ -16,10 +16,10 @@ function App(): JSX.Element {
     return (
         <Router history={history}>
             <section className="background h-screen overflow-y-hidden">
-                <nav className="absolute bg-transparent left-4 top-28">
+                <nav className="absolute bg-transparent left-4 top-28 z-10">
                     <Navigation />
                 </nav>
-                <aside className="absolute bg-transparent right-4 top-28">
+                <aside className="absolute bg-transparent right-4 top-28 z-10">
                     <Drawer />
                 </aside>
                 <main className="h-screen overflow-y-auto">
