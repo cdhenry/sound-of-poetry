76420178bf8f424eca3a100451cfbefdfc88f4db
Dan O'Meara <omeara.dan@gmail.com>
2021-04-25 17:45:13 -0700

    feat: working implementation needs cleaning up

--- client/src/components/atoms/MapChart.js
+++ client/src/components/atoms/MapChart.js
@@ -1,34 +1,41 @@
-import { geo, geoPath } from 'd3';
+import axios from 'axios';
+import { geoPath, scaleQuantize, schemeGreens } from 'd3';
 import { feature } from 'topojson-client';
 import React, { useState, useEffect } from 'react';
 
 
-const MapChart = () => {
-    const [geographies, setGeographies] = useState([])
+function MapChart({data}) {
+    const [regions, setRegions] = useState([])
 
     useEffect(() => {
-        fetch("/poet_regions_topo.json")
-            .then(response => {
-                if (response.status !== 200) {
-                    console.log(`There was a problem: ${response.status}`)
+        axios.get("poet_regions_topo.json")
+            .then(topoFile => {
+                if (topoFile.status !== 200) {
+                    console.log(`There was a problem: ${topoFile.status}`)
                     return
                 }
-                response.json().then(world => {
-                    setGeographies(feature(world, world.objects.regions).features)
-                })
+                const mapData = topoFile.data;
+                setRegions(feature(mapData, mapData.objects.regions).features)
             })
-    }, [])
+    }, [regions, setRegions])
+
+
+    const color = scaleQuantize([1, 600], schemeGreens[9]);
+    const recordColors = new Map();
+    data.forEach(record => {
+        recordColors.set(record.region, color(record.count_poet));
+    });
 
     return (
         <svg width={ 800 } height={ 450 } viewBox="0 0 800 450">
             <g className="states">
                 {
-                    geographies.map((d,i) => (
+                    regions.map((d, i) => (
                         <path
                             key={ `path-${ i }` }
                             d={ geoPath()(d) }
                             className="region"
-                            fill={ `rgba(100,50,56,${ 1 / geographies.length * i})` }
+                            fill={ `${recordColors.get(d.id)}` }
                             stroke="#FFFFFF"
                             strokeWidth={ 0.5 }
                         />
