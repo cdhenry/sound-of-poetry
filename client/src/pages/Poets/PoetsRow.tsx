b65265311666464361ba620145df38cf8549acf2
Chris <cdhenry@Chriss-MBP.lan>
2021-04-29 00:48:38 -0500

    fix: make list row linkable to poet page

--- client/src/pages/Poets/PoetsRow.tsx
+++ client/src/pages/Poets/PoetsRow.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useRouteMatch } from 'react-router-dom';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 
 import TableListItem from '../../components/atoms/TableListItem';
 import TableListRow from '../../components/molecules/TableListRow';
@@ -8,12 +8,15 @@ import { IPoetTableRowProps } from '../../interfaces/poet';
 export default function PoetsRow(props: IPoetTableRowProps): JSX.Element {
     const { item } = props
     const { url } = useRouteMatch()
+    const history = useHistory()
+
+    function handleRowClick() {
+        history.push(`${url}/${item.id}`)
+    }
 
     return (
-        <TableListRow className="cursor-pointer">
-            <TableListItem>
-                <Link to={{ pathname: `${url}/${item.id}` }}>{item.name}</Link>
-            </TableListItem>
+        <TableListRow className="cursor-pointer" onClick={handleRowClick}>
+            <TableListItem>{item.name}</TableListItem>
             <TableListItem>{item.region}</TableListItem>
             <TableListItem>{item.school}</TableListItem>
             <TableListItem>
