29af9eda55da7208d721a6eecf32437b93c25db5
jonathan-fuchs <jgfuchs@gmail.com>
2021-04-28 20:43:48 -0400

    feat: Poets page search and Poets bio pages (#30)
    * feat: Poets page search feature --- in progress
    
    * fix: table width styles
    
    * feat: poet bio page --- in progress
    
    * feat: Poet bio page
    
    * feat: poet filter --- work in progress
    
    * fix: selected options param added to poets route
    
    * feat: implemented poet name filter on Poets page
    
    * feat: Poet homepage and Poet bio pages
    
    * feat: implemented word wrap of poet bio page using tailwind
    
    * feat: added url to poet search and external link redirection
    
    * feat: changed url to open new tab on poetryfoundation website
    
    * feat: changed url to open new tab on poetryfoundation website
    
    * fix: word-wrap now works in Chromium-based browswers
    
    Co-authored-by: Chris <cdhenry@Chriss-MBP.lan>

--- client/src/services/poet.ts
+++ client/src/services/poet.ts
@@ -1,16 +1,18 @@
-import { IGetPoetNames, IPoet } from '../interfaces/poet'
-import { IPaginatedList, IPaginatedParams, ISelectOption } from '../interfaces/shared'
-import API from './api'
+import { IGetNamesParams, IGetPoetNames, IGetPoetsQuery, IPoet, IPoetTag } from '../interfaces/poet';
+import { IPaginatedList, IPaginatedParams, ISelectOption } from '../interfaces/shared';
+import API from './api';
 
 export class PoetService {
     private _baseUrl: string = 'poets/'
 
-    public async getPoets(params: IPaginatedParams) {
+    public async getPoets(params: IPaginatedParams, query?: IGetPoetsQuery) {
+        params = { ...params, ...query }
         let response = await API.get(`${this._baseUrl}`, { params })
         return response.data as IPaginatedList<IPoet>
     }
 
-    public async getPoetNames(params: IGetPoetNames) {
+    public async getPoetNames(params: IGetPoetNames, query?: IGetPoetsQuery) {
+        params = { ...params, ...query }
         let response = await API.get(`${this._baseUrl}names`, { params })
         return response.data as ISelectOption[]
     }
@@ -23,6 +25,11 @@ export class PoetService {
     public async getPoet(id: number) {
         let response = await API.get(`${this._baseUrl}${id}`)
         return response.data as IPoet
+    }
+
+    public async getNames(params?: IGetNamesParams) {
+        const response = await API.get(`${this._baseUrl}names`, { params })
+        return response.data as ISelectOption[] | IPoetTag[]
     }
 }
 
