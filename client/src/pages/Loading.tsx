7266fd0c234e9011d3cf110756cda7b3e2dc5b82
Chris <cdhenry@Chriss-MBP.lan>
2021-04-29 00:34:53 -0500

    fix: center loading component

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Loading(): JSX.Element {
-    return <section className="flex justify-center items-center h-screen">Loading...</section>
+    return <section className="flex">Loading...</section>
 }
